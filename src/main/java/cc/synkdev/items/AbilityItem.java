f0eacb17883b66cbe14a625eebd614ca734022e4
Synk_ <81869570+SynkMC@users.noreply.github.com>
2024-08-16 10:50:17 +0200

    Added contents to the Javadocs

--- src/main/java/cc/synkdev/items/AbilityItem.java
+++ src/main/java/cc/synkdev/items/AbilityItem.java
@@ -23,8 +23,8 @@ public class AbilityItem {
     ClickActions<PlayerInteractEvent> event;
 
     /**
-     *
-     * @param id Initialize the item
+     * Initialize the item
+     * @param id The ID you want to give to the item
      */
     public AbilityItem(String id) {
         this.setId("id");
@@ -32,6 +32,12 @@ public class AbilityItem {
     public static AbilityItem newItem(String id) {
         return new AbilityItem(id);
     }
+
+    /**
+     * Set what itemStack will be represented by the AbilityItem
+     * @param item The ItemStack
+     * @return
+     */
     public AbilityItem from(ItemStack item) {
         NBTItem nbtItem = new NBTItem(item);
         nbtItem.setString("abilityId", this.getId());
@@ -39,31 +45,68 @@ public class AbilityItem {
         this.setItemStack(item);
         return this;
     }
+
+    /**
+     * Stops the item from being moved from the slot it was in originally (except with openInv, death or /clear
+     * @return
+     */
     public AbilityItem lockInSlot() {
         this.setLocked(true);
         return this;
     }
+
+    /**
+     * Stops the item from being dropped to the ground
+     * @return
+     */
     public AbilityItem denyDrop() {
         this.setAllowDrop(false);
         return this;
     }
+
+    /**
+     * Sets what happens when the item is clicked
+     * @param event An example event for you to work on
+     * @return
+     */
     public AbilityItem setClickEvent(ClickActions<PlayerInteractEvent> event) {
         this.setEvent(event);
         return this;
     }
+
+    /**
+     * Set the stack size of the item
+     * @param quantity Stack size
+     * @return
+     */
     public AbilityItem setQuantity(int quantity) {
         this.getItemStack().setAmount(quantity);
         return this;
     }
+
+    /**
+     * Sets the permission required to use the item.
+     * @param permission
+     * @return
+     */
     public AbilityItem setPermission(String permission) {
         this.setPermission(permission);
         return this;
     }
+
+    /**
+     * Submit the item to the plugin
+     * @return
+     */
     public AbilityItem build() {
         AbilityItemsAPI.pluginsItems.add(this);
         return this;
     }
 
+    /**
+     * Give the item to a player
+     * @param p The player
+     */
     public void give(Player p) {
         p.getInventory().addItem(this.getItemStack());
     }
